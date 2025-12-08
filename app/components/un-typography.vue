763296c7039d58cfc80df3d08def3f194d1b2173
Yoones Khoshghadam <yooneskh@gmail.com>
2025-12-08 15:42:09 +0300

    fix (typography): better align the append slot

--- app/components/un-typography.vue
+++ app/components/un-typography.vue
@@ -45,38 +45,39 @@ const shouldShow = computed(() => {
   <div v-if="shouldShow">
 
     <div v-if="props.title || props.subtitle || props.icon || slots.append" class="flex items-start">
-      <div v-if="props.title || props.icon" class="flex items-center" style="gap: 0.5em;">
-        <u-icon
-          v-if="props.icon"
-          :name="props.icon"
-          :class="props.iconClasses"
-          style="width: 1.3em; height: 1.3em; flex-shrink: 0; flex-grow: 0;"
-        />
-        <h1
-          v-if="props.title"
-          class="font-medium"
-          :class="props.titleClasses"
-          style="font-size: 1.3em;">
-          {{ props.title }}
-        </h1>
+      <div>
+        <div v-if="props.title || props.icon" class="flex items-center" style="gap: 0.5em;">
+          <u-icon
+            v-if="props.icon"
+            :name="props.icon"
+            :class="props.iconClasses"
+            style="width: 1.3em; height: 1.3em; flex-shrink: 0; flex-grow: 0;"
+          />
+          <h1
+            v-if="props.title"
+            class="font-medium"
+            :class="props.titleClasses"
+            style="font-size: 1.3em;">
+            {{ props.title }}
+          </h1>
+        </div>
+        <h2
+          v-if="props.subtitle"
+          class="font-light"
+          :class="props.subtitleClasses"
+          style="font-size: 0.9em;"
+          :style="{
+            marginInlineStart: props.icon ? '2em' : '0',
+          }">
+          {{ props.subtitle }}
+        </h2>
       </div>
-      <template v-if="$slots.append">
-        <div class="ms-auto flex items-start gap-2">
+      <template v-if="slots.append">
+        <div class="ms-auto flex items-start gap-1">
           <slot name="append" />
         </div>
       </template>
     </div>
-
-    <h2
-      v-if="props.subtitle"
-      class="font-light"
-      :class="props.subtitleClasses"
-      style="font-size: 0.9em;"
-      :style="{
-        marginInlineStart: props.icon ? '2em' : '0',
-      }">
-      {{ props.subtitle }}
-    </h2>
 
     <p
       v-if="props.text"
