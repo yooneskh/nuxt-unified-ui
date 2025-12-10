6af69d18abeec6fdba469e36306788c240d30fda
Yoones Khoshghadam <yooneskh@gmail.com>
2025-12-10 21:24:34 +0300

    feat (un form): add media element

--- app/components/un-form.vue
+++ app/components/un-form.vue
@@ -16,6 +16,7 @@ import FormElementSelect from '../elements/form-element-select.vue';
 import FormElementSeries from '../elements/form-element-series.vue';
 import FormElementCheckbox from '../elements/form-element-checkbox.vue';
 import FormElementDate from '../elements/form-element-date.vue';
+import FormElementMedia from '../elements/form-element-media.vue';
 
 
 const elementsMap = {
@@ -25,6 +26,7 @@ const elementsMap = {
   'series': FormElementSeries,
   'date': FormElementDate,
   'checkbox': FormElementCheckbox,
+  'media': FormElementMedia,
 };
 
 
